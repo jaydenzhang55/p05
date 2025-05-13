9d3f404ba9849d0338b5be8abc6d877058bbc8c7
Danny Huang <dhuang50@stuy.edu>
2025-05-13 11:46:03 -0400

    login + register and navbar in index

--- app/__init__.py
+++ app/__init__.py
@@ -20,16 +20,59 @@ app = Flask(__name__)
 secret = os.urandom(32)
 app.secret_key = secret
 
+def signed_in():
+    return 'username' in session.keys() and session['username'] is not None
+
+def check_user(username):
+    user = db.getUser(username)
+    print(user)
+    if user is None:
+        return False
+    return user[0] == username
+
+def check_password(username, password):
+    user = db.getHash(username)
+    if user is None:
+        return False
+    print(user)
+    return user[0] == password
+
 @app.route('/', methods=['GET', 'POST'])
 def main():
     return render_template("index.html")
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+     if signed_in():
+        return redirect('/')
+     elif request.method == "POST":
+        username = request.form.get('username')
+        password = request.form.get('pw')
+        if not check_user(username):
+            return render_template("signIn.html", message="No such username exists")
+        if not check_password(username, password):
+            return render_template("signIn.html", message="Incorrect password")
+        session['username'] = username
+        session["password"] = request.form.get("pw")
+        return redirect('/')
+     return render_template("login.html")
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if signed_in():
+        return redirect('/')
+    elif request.method == "POST":
+        username = request.form['username']
+        password = request.form['pw']     
+        user = db.getUser(username)
+        if user is None:
+            db.addUser(name, username, password)
+            session["username"] = username
+            session["password"] = password
+            return redirect('/login')
+        else:
+            return render_template('register.html', message="Username already exists")
     return render_template("register.html")
 
 @app.route('/saved/<username>', methods=['GET', 'POST'])
@@ -45,8 +88,8 @@ def search():
     return render_template("search.html")
 
 if __name__ == "__main__":
-    app.debug = False
-    app.run(host='0.0.0.0')
-    #app.debug = True
-    #app.run(host='127.0.0.1')
+#     app.debug = False
+#     app.run(host='0.0.0.0')
+    app.debug = True
+    app.run(host='127.0.0.1')
     
