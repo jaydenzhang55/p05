5481896accc8a47e93d00ccf4544a62f8aae58a6
projectjayden <jaydenz0704@gmail.com>
2025-05-29 09:20:10 -0400

    final?

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,7 @@ from flask import url_for
 from flask import send_file
 from flask import Response
 from io import BytesIO
-from .Solutions import *
+from Solutions import *
 import pikepdf
 import requests
 from bs4 import BeautifulSoup
