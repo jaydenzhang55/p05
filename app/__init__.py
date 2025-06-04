6284e8254e89c26d871419610a6d339edc52febc
Jayden Zhang <117393725+jaydenzhang55@users.noreply.github.com>
2025-06-04 11:19:24 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -53,10 +53,9 @@ def check_password(username, password):
 @app.route('/', methods=['GET', 'POST'])
 def main():
     if signed_in():
-        return render_template("index.html", loggedIn=True, username=session['username'])
+        return render_template("index.html", loggedIn="true", username=session['username'])
     else:
-        return render_template("index.html", loggedIn=False, username='')
-
+        return render_template("index.html", loggedIn="false", username='')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -66,32 +65,32 @@ def login():
         username = request.form.get('username')
         password = request.form.get('pw')
         if not check_user(username):
-            return render_template("login.html", message="No such username exists", loggedIn=True)
+            return render_template("login.html", message="No such username exists", loggedIn="false")
         if not check_password(username, password):
-            return render_template("login.html", message="Incorrect password", loggedIn=False)
+            return render_template("login.html", message="Incorrect password", loggedIn="false")
         session['username'] = username
         session["password"] = request.form.get("pw")
         return redirect('/')
-     return render_template("login.html", loggedIn=False)
+     return render_template("login.html", loggedIn="false")
 
 @app.route('/solution', methods=['GET', 'POST'])
 def solution():
     if not signed_in():
         return render_template("login.html", message="Not logged in!", loggedIn="false")
-
-    video = None 
-    explanation = None
-    prompt = ""
-
-    if request.method == "POST":
-        api_key = getAIKey()
-        prompt = request.form.get("prompt", "")
-        if api_key and prompt:
-            explanation = sol.getGeminiExplaination(api_key, prompt)
-            video = sol.getGeminiVideo(api_key, prompt)
-
-    return render_template("solutions.html",username = session['username'], explanation=explanation, prompt=prompt, video=video, loggedIn="true")
-
+    else:
+        video = None 
+        explaination = None
+        prompt = ""
+        if request.method == "POST":
+            #lowkey idk how to hide API keys
+            
+            api_key = getAIKey()
+            prompt = request.form.get("prompt", "")
+            if api_key and prompt:
+                explanation = sol.getGeminiExplaination(api_key, prompt)
+                video_count = sol.getGeminiVideo(api_key, prompt)
+    
+    return render_template("solutions.html", explanation=explanation, prompt=prompt, video=video, loggedIn="true")
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -107,15 +106,15 @@ def register():
             session["password"] = password
             return redirect('/login')
         else:
-            return render_template('register.html', message="Username already exists", loggedIn=False)
-    return render_template("register.html", loggedIn=False)
+            return render_template('register.html', message="Username already exists", loggedIn="false")
+    return render_template("register.html", loggedIn="false")
 
 @app.route('/saved/<username>', methods=['GET', 'POST'])
 def saved(username):
     if signed_in():
-        return render_template("saved.html", loggedIn=True, username=session['username'])
+        return render_template("saved.html", loggedIn="true", username=session['username'])
     else:
-        return render_template("saved.html", loggedIn=False, username='')
+        return render_template("saved.html", loggedIn="false", username='')
 
 @app.route('/book', methods=['GET', 'POST'])
 def book():
@@ -125,9 +124,9 @@ def book():
     if pdf_data:
         pdf_b64 = base64.b64encode(pdf_data).decode('utf-8')
     if signed_in():
-            return render_template("book.html", loggedIn=True, username=session['username'], title=title, pdf_b64=pdf_b64)
+            return render_template("book.html", loggedIn="true", username=session['username'], title=title, pdf_b64=pdf_b64)
     else:
-        return render_template("book.html", loggedIn=False, username='', title=title, pdf_b64=pdf_b64)
+        return render_template("book.html", loggedIn="false", username='', title=title, pdf_b64=pdf_b64)
     
 @app.route('/search', methods=['GET', 'POST'])
 def search():
@@ -144,15 +143,15 @@ def search():
             boolean = True
 
         if signed_in():
+            print(session['username'])
             return render_template(
-                "search.html",loggedIn=True,search=search_term, searchFound = boolean, list=lists, boolean=boolean, username=session["username"]
+                "search.html",loggedIn="true",search=search_term, searchFound = boolean, list=lists, boolean=boolean, username=session["username"]
             )
         else:
             # Not signed in
             return render_template(
-                "search.html",loggedIn=False,search=search_term,searchFound=boolean, list=lists, boolean=boolean,username=None
+                "search.html",loggedIn="false",search=search_term,searchFound=boolean, list=lists, boolean=boolean,username=None
             )
-
 
 @app.route('/logout', methods=['GET', 'POST'])
 def logOut():
@@ -182,26 +181,30 @@ def websiteLinkCreator(query):
     queryArray = query.split(' ')
     modification = ""
     for _ in range(len(queryArray)):
-        modification = queryArray[_]
+        if "'" in queryArray[_]:
+            queryArray[_].replace("'", "%27")
+        modification = queryArray[_] + "+"
     URL = "https://annas-archive.org/search?q=" + modification
     page = requests.get(URL)
 
     soup = BeautifulSoup(page.content, "html.parser")
 
-    for link in soup.find_all('a'):
-        print(link.get('href'))
-
-    return soup.find_all('a')
+    try:
+        for link in soup.find_all('a'):
+            print(link.get('href'))
+        return soup.find_all('a')
+    except:
+        return "no books found"
 
 def getDownloadPDFLink(chosenLink): 
-    URL = "https://annas-archive.org" + chosenLink
+    URL = "https://archive.org" + chosenLink
     page = requests.get(URL)
 
     soup = BeautifulSoup(page.content, "html.parser")
     listOfLinks = []
 
     for link in soup.find_all('a'):
-        if "slow" in link:
+        if "pdf" in link:
             listOfLinks.append(link)
 
     return listOfLinks
@@ -215,7 +218,7 @@ def PDF(chosenLink, query):
     listOfLinks = getDownloadPDFLink(chosenLink)
     originalPath = "downloaded.pdf"
     compressedPath = "compressed.pdf"
-    download_pdf_file("https://annas-archive.org" + listOfLinks[0], originalPath)
+    download_pdf_file("https://archive.org" + listOfLinks[0], originalPath)
     compress_pdf_pikepdf(originalPath, compressedPath)
     db.storePDF(query, compressedPath, None)
 
@@ -232,4 +235,3 @@ if __name__ == "__main__":
 #     app.run(host='0.0.0.0')
     app.debug = True
     app.run(host='127.0.0.1')
-
