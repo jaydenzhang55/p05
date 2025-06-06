5fb76d39b72bf1ccd63f93acf9141b2f8bf99173
projectjayden <jaydenz0704@gmail.com>
2025-06-06 01:32:08 -0400

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -97,14 +97,14 @@ def login():
         username = request.form.get('username')
         password = request.form.get('pw')
         if not check_user(username):
-            flash("No such username exists!")
+            flash("No such username exists!", 'error')
             return render_template("login.html", loggedIn=False, all=all)
         if not check_password(username, password):
-            flash("Incorrect password")
+            flash("Incorrect password!", 'error')
             return render_template("login.html", loggedIn=False, all=all)
         session['username'] = username
         session["password"] = request.form.get("pw")
-        flash("Successfully created account!")
+        flash("Successfully created account!", 'success')
         return redirect('/')
      return render_template("login.html", loggedIn=False, all=all)
 
@@ -143,7 +143,8 @@ def register():
             session["password"] = password
             return redirect('/login')
         else:
-            return render_template('register.html', message="Username already exists", loggedIn=False, all=all)
+            flash("Username already exists!", 'error')
+            return render_template('register.html', loggedIn=False, all=all)
     return render_template("register.html", loggedIn=False, all=all)
 
 @app.route('/saved', methods=['GET', 'POST'])
@@ -202,9 +203,9 @@ def book():
             explanation = sol.getGeminiMedia(api_key, uploaded_file)
 
     if not signed_in():
-        return render_template("book.html", loggedIn=False, username='', title=title, pdf_b64=pdf_b64, all=all, explanation=explanation, prompt=prompt, video=video )
+        return render_template("book.html", loggedIn=False, username='', title=title, pdf_b64=pdf_b64, all=all, explanation=explanation, prompt=prompt, video=video)
     else:
-        return render_template( "book.html", username=session.get('username'), loggedIn=True, title=title, pdf_b64=pdf_b64, all=all, explanation=explanation, prompt=prompt, video=video )
+        return render_template("book.html", username=session.get('username'), loggedIn=True, title=title, pdf_b64=pdf_b64, all=all, explanation=explanation, prompt=prompt, video=video, saves = db.getSaved(session.get('username')))
     
 @app.route('/search', methods=['GET', 'POST'])
 def search():
